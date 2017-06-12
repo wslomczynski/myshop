3112797daf7a7c311a958007c05e50e886a14920
wsl <w.sl@o2.pl>
2017-06-12 12:00:23 +0200

    changed config_prod to stop caching orm

---
+++ src/AppBundle/Controller/ProductsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductsController extends Controller
+{
+
+  /**
+   * @Route("/products")
+   */
+  public function indexAction(Request $request){
+
+    $noMoreProducts = false;
+    $category = NULL;
+
+    $products_per_page = 8;
+
+
+    $repository = $this->getDoctrine()->getRepository('AppBundle:Product');
+    $search_word = $request->query->get('search_word');
+
+
+    if($search_word){
+      $products = $repository->findByName($search_word);
+    } else {
+      $products = $repository->findAll();
+    }
+
+
+
+
+
+    $firstProducts = array_slice($products,0,$products_per_page);
+
+    if($request->isXmlHttpRequest()){
+
+      $latest_post_id = $request->get('latest_post_id');                      // getting highest data-post-id atrribute from products
+      $latest_post_id = (int)$latest_post_id;                                 // convert latest_post_id form string to int
+      $latest_post_id++;
+
+      $next_products = array_slice($products,$latest_post_id,$products_per_page);
+
+
+
+      $html =  $this->renderView('products/next.html.twig',['next_products' => $next_products, 'latest_post_id' => $latest_post_id]); // rendering $html from twig template
+
+        //return new Response($html);
+
+      $items = array('html' => $html, 'noMoreProducts' => $noMoreProducts);
+      return new JsonResponse($items);
+        //return Response::create($html, 200,['noMoreProducts' => $noMoreProducts]);
+
+      }
+
+
+
+    return $this->render('products/index.html.twig',['products' => $firstProducts]);
+  }
+
+
+
+
+}
---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product", indexes={@ORM\Index(name="IDX_D34A04AD64C19C1", columns={"category"}), @ORM\Index(name="IDX_D34A04ADC53D045F", columns={"image"})})
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=10, scale=0, nullable=false)
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="product_id_seq", allocationSize=1, initialValue=1)
+     */
+    private $id;
+
+    /**
+     * @var \AppBundle\Entity\ProductCategory
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\ProductCategory")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="category", referencedColumnName="id")
+     * })
+     */
+    private $category;
+
+    /**
+     * @var \AppBundle\Entity\ProductImage
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\ProductImage")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="image", referencedColumnName="id")
+     * })
+     */
+    private $image;
+
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \AppBundle\Entity\ProductCategory $category
+     *
+     * @return Product
+     */
+    public function setCategory(\AppBundle\Entity\ProductCategory $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \AppBundle\Entity\ProductCategory
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set image
+     *
+     * @param \AppBundle\Entity\ProductImage $image
+     *
+     * @return Product
+     */
+    public function setImage(\AppBundle\Entity\ProductImage $image = null)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return \AppBundle\Entity\ProductImage
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
