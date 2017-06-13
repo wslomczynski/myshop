76794184c6d8ff113c3961592003722e2f941161
Wojciech Słomczyński <w.sl1@o2.pl>
2017-06-13 04:57:39 -0400

    removed marks from merge conflict

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -36,11 +36,7 @@ class AdminController extends Controller
        if($file){
 
          $img = new ProductImage();
-<<<<<<< HEAD
          $img->setName($file->getClientOriginalName());
-=======
-         $img->setImageName($file->getClientOriginalName());
->>>>>>> a9bbfc66ec8a58e5e69fac78c0014969e43c1fbf
          $em->persist($img);
          $em->flush();
 
@@ -49,21 +45,15 @@ class AdminController extends Controller
          $filename = $file->getClientOriginalName();
          $file->move($path_to_save,$filename);
 
-<<<<<<< HEAD
          $params['category'] = $category_repository->find(intval($params['category']));
-=======
-         //$params['category'] = $category_repository->find(intval($params['category']));
->>>>>>> a9bbfc66ec8a58e5e69fac78c0014969e43c1fbf
+
          //$category = new ProductCategory();
          $product = new Product();
          $product->setName($params['name']);
          $product->setPrice($params['price']);
          $product->setDescription($params['description']);
-<<<<<<< HEAD
+
          $product->setCategory($params['category']);
-=======
-         $product->setCategory(new ProductCategory());
->>>>>>> a9bbfc66ec8a58e5e69fac78c0014969e43c1fbf
          $product->setImage($img);
 
 
